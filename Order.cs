c4b3a112acc0754fcbd4e21e5b69f1a0b48de982
bukovsky <vojbukovsky@gmail.com>
2025-04-13 19:10:03 +0200

    Classes Order, OrderItems

---
+++ Order.cs
@@ -0,0 +1,14 @@
+﻿namespace BukovskyCaseStudy
+{
+    public class Order
+    {
+        public Guid OrderNumber { get; set; }
+        public string? ClientName { get; set; }
+        public DateTime DateCreated { get; set; }
+        public List<OrderItem> OrderItems { get; set; }
+        public Order()
+        {
+            this.OrderItems = [];
+        }
+    }
+}
---
+++ OrderItem.cs
@@ -0,0 +1,9 @@
+﻿namespace BukovskyCaseStudy
+{
+    public class OrderItem
+    {
+        public string ItemName { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+    }
+}
