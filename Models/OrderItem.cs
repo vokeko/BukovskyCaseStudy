576b1169410881b7ac51974f071969803772042b
bukovsky <vojbukovsky@gmail.com>
2025-04-14 17:15:42 +0200

    Initial SQLLite table creation

--- Models/OrderItem.cs
+++ Models/OrderItem.cs
@@ -9,5 +9,6 @@ namespace BukovskyCaseStudy.Models
         public string ItemName { get; set; }
         public decimal Price { get; set; }
         public int Quantity { get; set; }
+        public Guid OrderId { get; set; }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,5 +1,8 @@
 using Microsoft.EntityFrameworkCore;
 using BukovskyCaseStudy.Models;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore.Migrations.Operations;
+using System.Runtime.CompilerServices;
 
 var builder = WebApplication.CreateBuilder(args);
 var configuration = builder.Configuration;
@@ -22,7 +25,27 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
-
 app.MapControllers();
+
+SqliteConnection m_dbConnection = new SqliteConnection(configuration.GetConnectionString("DefaultConnection"));
+m_dbConnection.Open();
+string sql = @"CREATE TABLE IF NOT EXISTS Orders (
+    Id TEXT PRIMARY KEY,
+    ClientName TEXT,
+    DateCreated TEXT NOT NULL,
+    Status INTEGER NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS OrderItems (
+    Id INTEGER PRIMARY KEY,
+    OrderId TEXT NOT NULL,
+    ItemName TEXT NOT NULL,
+    Price REAL NOT NULL,
+    Quantity INTEGER NOT NULL,
+    FOREIGN KEY (OrderId) REFERENCES Orders(Id) ON DELETE CASCADE
+);";
+SqliteCommand command = new SqliteCommand(sql, m_dbConnection);
+command.ExecuteNonQuery();
+m_dbConnection.Close();
 
 app.Run();
