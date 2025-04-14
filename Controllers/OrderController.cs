09a3592227bb75cc6c0fd22df930f6b762529cbf
bukovsky <vojbukovsky@gmail.com>
2025-04-14 17:19:12 +0200

    dbcontext injection for ordercontroller, routing changes, get order

--- Controllers/OrderController.cs
+++ Controllers/OrderController.cs
@@ -1,36 +1,46 @@
-﻿using System.Net;
-using Microsoft.AspNetCore.Http;
+﻿using BukovskyCaseStudy.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace BukovskyCaseStudy.Controllers
 {
     [ApiController]
+    [Route("api/Orders")]
     public class OrderController : ControllerBase
     {
+
+        private readonly OrderDbContext _dbContext;
         private readonly ILogger<OrderController> _logger;
 
-        public OrderController(ILogger<OrderController> logger)
+        public OrderController(ILogger<OrderController> logger, OrderDbContext dbContext)
         {
             _logger = logger;
+            _dbContext = dbContext;
         }
 
-        [Route("[controller]/Orders")]
+        [Route("")]
         [HttpGet(Name = "GetOrderList")] 
         public List<Models.Order> GetOrderList()
         {
             return new List<Models.Order>();
         }
 
-        [Route("[controller]/Orders")]
+        [Route("")]
         [HttpPost(Name = "CreateOrder")]
         public void CreateOrder()
         {
         }
 
-        [Route("[controller]/Orders/{id}")]
+        [Route("{id:guid}")]
         [HttpPatch(Name = "ProcessOrder")]
         public void ProcessOrder(bool isPaid)
+        private Order? GetOrderById(Guid id)
         {
+            return _dbContext.Orders.SingleOrDefault(o => o.Id == id);
+        }
+        private bool OrderExists(Guid id)
+        {
+            return _dbContext.Orders.Any(o => o.Id == id);
         }
     }
 }
