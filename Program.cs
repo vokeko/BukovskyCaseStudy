ef2cf6f3b1aed8762a2def73fe3a8fd5112baafa
voke <vojbukovsky@gmail.com>
2025-04-16 19:49:01 +0200

    Migration

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using BukovskyCaseStudy.Models;
 using Microsoft.Data.Sqlite;
 using Microsoft.EntityFrameworkCore.Migrations.Operations;
@@ -23,26 +23,5 @@ app.UseHttpsRedirection();
 
 app.UseAuthorization();
 app.MapControllers();
-
-SqliteConnection m_dbConnection = new SqliteConnection(configuration.GetConnectionString("DefaultConnection"));
-m_dbConnection.Open();
-string sql = @"CREATE TABLE IF NOT EXISTS Orders (
-    Id TEXT PRIMARY KEY,
-    ClientName TEXT,
-    DateCreated TEXT NOT NULL,
-    Status INTEGER NOT NULL
-);
-
-CREATE TABLE IF NOT EXISTS OrderItems (
-    Id INTEGER PRIMARY KEY,
-    OrderId TEXT NOT NULL,
-    ItemName TEXT NOT NULL,
-    Price REAL NOT NULL,
-    Quantity INTEGER NOT NULL,
-    FOREIGN KEY (OrderId) REFERENCES Orders(Id) ON DELETE CASCADE
-);";
-SqliteCommand command = new SqliteCommand(sql, m_dbConnection);
-command.ExecuteNonQuery();
-m_dbConnection.Close();
 
 app.Run();
